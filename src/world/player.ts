7bb321b372ebd1ffbf77225ce24f44c8302c2780
boogie-nights <54244688+boogie-nights@users.noreply.github.com>
2022-10-19 12:38:43 -0400

    Send Side Bar Widget Packet

--- src/world/player.ts
+++ src/world/player.ts
@@ -1,12 +1,13 @@
 import { Client } from '../net/client';
 import { Coord } from './coord';
-import { sendUpdateMapRegionPacket } from '../net/packets';
+import { sendSideBarWidget, sendUpdateMapRegionPacket } from '../net/packets';
 
 export enum PlayerRights {
     USER = 0,
     MOD = 1,
     JMOD = 2,
 }
+
 
 export interface Player {
     uid: number;
@@ -15,6 +16,7 @@ export interface Player {
     rights: PlayerRights;
     client: Client;
     position?: Coord;
+    sideBarData: number[];
 }
 
 export const playerLogin = (player: Player): void => {
@@ -24,5 +26,11 @@ export const playerLogin = (player: Player): void => {
         plane: 0,
     };
 
+    player.sideBarData = [ 2423, 3917, 638, 3213, 1644, 5608, 1151, 65535, 5065, 5715, 2449, 904, 147, 962 ];
+
     sendUpdateMapRegionPacket(player); // @todo move to player sync when available - Kat 18/Oct/22
+    
+    player.sideBarData.forEach((id, index) => {
+        sendSideBarWidget(player, index, id);
+    })
 };
