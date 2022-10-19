52454374669b476e9654f9f7230642ed2a410f8c
boogie-nights <54244688+boogie-nights@users.noreply.github.com>
2022-10-19 12:57:38 -0400

    System Update Packet and widgetState

--- src/net/packets.ts
+++ src/net/packets.ts
@@ -67,6 +67,11 @@ export const sendSideBarWidget = (player: Player, sideBarId: number, widgetId: n
     const buffer = new ByteBuffer(3);
     buffer.put(widgetId, 'short');
     buffer.put(sideBarId - 128, 'byte'); // @todo Subtracting 128 because this is a byteA, need to refactor out A, S, and C from the client buffer. - Brian 10-19-22
-
     writePacket(player, 229, buffer);
-}
+};
+
+export const sendSystemUpdate = (player: Player, time: number): void => {
+    const buffer = new ByteBuffer(2)
+    buffer.put(time, 'short');
+    writePacket(player, 103, buffer);
+};
--- src/world/player.ts
+++ src/world/player.ts
@@ -1,6 +1,6 @@
 import { Client } from '../net/client';
 import { Coord } from './coord';
-import { sendSideBarWidget, sendUpdateMapRegionPacket } from '../net/packets';
+import { sendSideBarWidget, sendSystemUpdate, sendUpdateMapRegionPacket } from '../net/packets';
 
 export enum PlayerRights {
     USER = 0,
@@ -8,6 +8,9 @@ export enum PlayerRights {
     JMOD = 2,
 }
 
+export interface WidgetState {
+    sideBarData: number[];
+}
 
 export interface Player {
     uid: number;
@@ -16,7 +19,7 @@ export interface Player {
     rights: PlayerRights;
     client: Client;
     position?: Coord;
-    sideBarData: number[];
+    widgetState?: WidgetState;
 }
 
 export const playerLogin = (player: Player): void => {
@@ -26,11 +29,14 @@ export const playerLogin = (player: Player): void => {
         plane: 0,
     };
 
-    player.sideBarData = [ 2423, 3917, 638, 3213, 1644, 5608, 1151, 65535, 5065, 5715, 2449, 904, 147, 962 ];
+    player.widgetState = {
+        sideBarData: [ 2423, 3917, 638, 3213, 1644, 5608, 1151, 65535, 5065, 5715, 2449, 904, 147, 962 ]
+    };
 
     sendUpdateMapRegionPacket(player); // @todo move to player sync when available - Kat 18/Oct/22
+    sendSystemUpdate(player, 100);
     
-    player.sideBarData.forEach((id, index) => {
+    player.widgetState.sideBarData.forEach((id, index) => {
         sendSideBarWidget(player, index, id);
-    })
+    });
 };
