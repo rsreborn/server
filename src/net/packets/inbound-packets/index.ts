e398e2db34b31fbcb11e05d03d3207c0a4958452
boogie-nights <54244688+boogie-nights@users.noreply.github.com>
2022-11-22 16:44:48 -0500

    Command Packet for 319

--- src/net/packets/inbound-packets/index.ts
+++ src/net/packets/inbound-packets/index.ts
@@ -2,11 +2,13 @@ import { buttonPacket } from './button-packet';
 import { clickPacket } from './click-packet';
 import { walkPacket } from './walk-packet';
 import { InboundPacket } from '../packets';
+import { commandPacket } from './command-packet';
 
 const inboundPackets: InboundPacket[] = [
     buttonPacket,
     clickPacket,
     walkPacket,
+    commandPacket,
 ];
 
 export default inboundPackets;
