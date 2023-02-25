8203f93c4c42774183cef65c824c7a0dc4f2a3a4
boogie-nights <brian.spray93@gmail.com>
2023-02-25 11:59:03 -0500

    Attempt at sounds

--- src/engine/net/packets/outbound-packets/encoders/just-testing-packet.ts
+++ src/engine/net/packets/outbound-packets/encoders/just-testing-packet.ts
@@ -12,15 +12,16 @@ interface ProjectileData {
 export const justTestingPacket: OutboundPacket<ProjectileData> = {
     name: 'justTesting',
     opcodes: {
-        254: 88,
+        254: 25,
     },
     encoders: {
         254: (player, opcode, data) => {
-            sendUpdateCoords(player, 3219, 3222)
+           // sendUpdateCoords(player, 3219, 3222)
            
-            const buffer = new ByteBuffer(2);
-            buffer.put(0, 'byte');
-            buffer.put((10 << 2) + (3 & 3), 'byte');
+            const buffer = new ByteBuffer(5);
+            buffer.put(5, 'short');
+            buffer.put(10, 'byte');
+            buffer.put(5, 'short');
             //buffer.put(86, 'short');
            // buffer.put(0, 'short');
 
